7671d6c6bb628ee463ef59bf3135cd38bd6feb75
jakkappro <jakkappro@gmail.com>
2020-09-29 09:15:57 +0200

    vsetko

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,23 @@
 package com.appslab;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Please put height of tower: ");
+        double height_tower = myObj.nextDouble();
+
+        System.out.println("Please put height of step: ");
+        double height_step = myObj.nextDouble();
+
+        System.out.println("Please put lenght of step: ");
+        double lenght_step = myObj.nextDouble();
+
+        double distance = height_tower / height_step * (height_step + lenght_step);
+        System.out.println("Snail is going to travel: " + distance);
+
     }
 }
