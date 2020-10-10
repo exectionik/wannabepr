7b4dc58e704608dcb494510e5b744368b5b54e2a
exectionik <exectionik9@gmail.com>
2020-10-10 19:57:00 +0200

    finally

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,46 @@
 package com.appslab;
 
+
+
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020! haa");
+
+        public static void main(String[] args) {
+
+            System.out.println(fciaaa(3, 3, 3));
+        }
+            public static int fciaaa(int a, int b, int c){
+                int eqe = 1;
+                if (a == b)
+                    eqe++;
+                if (a == c)
+                    eqe++;
+                if (c == b)
+                    eqe++;
+                if (a == b && a == c && b == c)
+                    eqe--;
+                return eqe;
+
+
+
+            }
+
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
