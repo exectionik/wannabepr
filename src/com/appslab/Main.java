0834b902d20112120b9aff0e1f873964dcab9520
exectionik <exectionik9@gmail.com>
2020-10-14 22:19:24 +0200

    stratil som skoro nervy u toho

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,29 +1,24 @@
 package com.appslab;
-
-
-
+import java.util.Arrays;
 
 public class Main {
 
+    public static int[] numbs(){
 
-        public static void main(String[] args) {
+        int[] ar = {5,6,9,8};
+        return ar;
 
-            System.out.println(fciaaa(2, 3, 4));
         }
-            public static boolean fciaaa(int a, int b, int c){
-                int eqe = 0;
-                boolean pepe = false;
-                if ((a + b > c) && (a + c > b) && (b + c > a))
-                    eqe++;
-                if ((a*a + b*b == c*c) || (b*b + c*c == a*a) || (c*c + a*a == b*b))
-                    eqe++;
-                if (eqe > 0)
-                    pepe = true;
-                return pepe;
 
 
+    public static void main(String[] args) {
+        int[] arr=numbs();
+        for (int i = 0; i< arr.length;i++){
+            System.out.println(arr[i] + " " + arr[i] * arr.length);
+        }
 
-            }
+    }
+
 
     }
 
